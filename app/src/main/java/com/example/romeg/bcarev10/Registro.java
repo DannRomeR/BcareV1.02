962b12c12b86b4a94837ae51882979a5eed7791e
romeg <romegerdan@gmail.com>
2017-11-16 07:57:33 -0600

    Se agrega aviso de privacidad

--- app/src/main/java/com/example/romeg/bcarev10/Registro.java
+++ app/src/main/java/com/example/romeg/bcarev10/Registro.java
@@ -1,5 +1,7 @@
 package com.example.romeg.bcarev10;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -540,6 +542,8 @@ public class Registro extends AppCompatActivity {
 
             if (v.getId() == R.id.btnregistrarreg)
             {
+
+
                 try {
                     if (namestr.isEmpty() || appstr.isEmpty() || apmstr.isEmpty() || emailstr.isEmpty() || unamestr.isEmpty() || pass1str.isEmpty() || pass2str.isEmpty() || edadstr.isEmpty() || telstr.isEmpty()) {
                         Toast.makeText(this, "Los campos marcados con '*' son obligatorios ",
@@ -580,47 +584,116 @@ public class Registro extends AppCompatActivity {
 
                     } else//***********************************REGISTRO****************************/
                         {
+                            AlertDialog.Builder dialogo1 = new AlertDialog.Builder(this);
+                            dialogo1.setTitle("Politica de privacidad");
+                            dialogo1.setMessage("El presente Política de Privacidad establece los términos en que B-Care" +
+                                    " usa y protege la información que es proporcionada por sus usuarios al momento de utilizar" +
+                                    " su sitio web. Esta compañía está comprometida con la seguridad de los datos de sus usuarios." +
+                                    " Cuando le pedimos llenar los campos de información personal con la" +
+                                    " cual usted pueda ser identificado, lo hacemos asegurando que sólo se " +
+                                    "empleará de acuerdo con los términos de este documento. Sin embargo esta" +
+                                    " Política de Privacidad puede cambiar con el tiempo o ser actualizada por" +
+                                    " lo que le recomendamos y enfatizamos revisar continuamente esta página para" +
+                                    " asegurarse que está de acuerdo con dichos cambios." +
+                                    "Información que es recogida\n" +
+                                    "\n" +
+                                    "Nuestro sitio web podrá recoger información personal por ejemplo: Nombre," +
+                                    "  información de contacto como  su dirección de correo electrónica e información demográfica." +
+                                    " Así mismo cuando sea necesario podrá ser requerida información específica para" +
+                                    " procesar algún pedido o realizar una entrega o facturación.\n" +
+                                    "\n" +
+                                    "Uso de la información recogida\n " +
+                                    "\nNuestro sitio web emplea la información con" +
+                                    " el fin de proporcionar el mejor servicio posible, particularmente para mantener un registro" +
+                                    " de usuarios, de pedidos en caso que aplique, y mejorar nuestros productos y servicios. " +
+                                    " Es posible que sean enviados correos electrónicos periódicamente a través de nuestro sitio " +
+                                    "con ofertas especiales, nuevos productos y otra información publicitaria que consideremos relevante " +
+                                    "para usted o que pueda brindarle algún beneficio, estos correos electrónicos serán enviados a la" +
+                                    " dirección que usted proporcione y podrán ser cancelados en cualquier momento.\n" +
+                                    "\n" +
+                                    "B-Care está altamente comprometido para cumplir con el compromiso de mantener su información " +
+                                    "segura. Usamos los sistemas más avanzados y los actualizamos constantemente para asegurarnos" +
+                                    " que no exista ningún acceso no autorizado.\n" +
+                                    "\nCookies\n" +
+                                    "\n" +
+                                    "Una cookie se refiere a un fichero que es enviado con la finalidad de solicitar permiso " +
+                                    "para almacenarse en su ordenador, al aceptar dicho fichero se crea y la cookie sirve " +
+                                    "entonces para tener información respecto al tráfico web, y también facilita las futuras" +
+                                    " visitas a una web recurrente. Otra función que tienen las cookies es que con ellas las" +
+                                    " web pueden reconocerte individualmente y por tanto brindarte el mejor servicio " +
+                                    "personalizado de su web.\n" +
+                                    "\n" +
+                                    "Nuestro sitio web emplea las cookies para poder identificar las páginas " +
+                                    "que son visitadas y su frecuencia. Esta información es empleada únicamente para análisis" +
+                                    " estadístico y después la información se elimina de forma permanente. Usted puede eliminar" +
+                                    " las cookies en cualquier momento desde su ordenador. Sin embargo las cookies ayudan a " +
+                                    "proporcionar un mejor servicio de los sitios web, estás no dan acceso a información de su " +
+                                    "ordenador ni de usted, a menos de que usted así lo quiera y la proporcione directamente," +
+                                    " link . Usted puede aceptar o negar el uso de cookies, sin embargo la mayoría de navegadores" +
+                                    " aceptan cookies automáticamente pues sirve para tener un mejor servicio web. " +
+                                    "También usted puede cambiar la configuración de su ordenador para declinar las cookies." +
+                                    " Si se declinan es posible que no pueda utilizar algunos de nuestros servicios.\n" +
+                                    "\n" +
+                                    "Enlaces a Terceros\n" +
+                                    "\nEste sitio web pudiera contener en laces a otros sitios que pudieran ser de su interés." +
+                                    " Una vez que usted de clic en estos enlaces y abandone nuestra página, ya no tenemos control" +
+                                    " sobre al sitio al que es redirigido y por lo tanto no somos responsables de los términos o" +
+                                    " privacidad ni de la protección de sus datos en esos otros sitios terceros. Dichos sitios " +
+                                    "están sujetos a sus propias políticas de privacidad por lo cual es recomendable que los " +
+                                    "consulte para confirmar que usted está de acuerdo con estas.\n" +
+                                    "\n" +
+                                    "Control de su información personal\n" +
+                                    "\n" +
+                                    "En cualquier momento usted puede restringir la recopilación o el uso de la información" +
+                                    " personal que es proporcionada a nuestro sitio web.  Cada vez que se le solicite rellenar" +
+                                    " un formulario, como el de alta de usuario, puede marcar o desmarcar la opción de recibir " +
+                                    "información por correo electrónico.  En caso de que haya marcado la opción de recibir " +
+                                    "nuestro boletín o publicidad usted puede cancelarla en cualquier momento.\n" +
+                                    "\n" +
+                                    "B-Care Se reserva el derecho de cambiar los términos de la presente Política de" +
+                                    " Privacidad en cualquier momento.");
+                            dialogo1.setCancelable(false);
+                            dialogo1.setNegativeButton("No acepto", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialogo1, int id) {
+                                    finish();
+                                }
+                            });
+                            dialogo1.setPositiveButton("Acepto", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialogo1, int id) {
+                                    String namestr = etnombre.getText().toString();
+                                    String appstr = etapp.getText().toString();
+                                    String apmstr = etapm.getText().toString();
 
-                            String nomS = namestr.substring(0, 1);
-                            String appS = appstr.substring(0, 1);
-                            String apmS = apmstr.substring(0, 1);
-                            int edadint = Integer.parseInt(edadstr);
-                            int telint = Integer.parseInt(telstr);
-                            int cont1int = Integer.parseInt(cont1str);
-                            int cont2int = Integer.parseInt(cont2str);
-                            int numa = (int) (Math.random() * 999) + 1;
-                            String numastr = String.valueOf(numa);
-                            String numPac = nomS + appS + apmS + edadstr + numastr;
+                                    String edadstr = etedad.getText().toString();
+                                    String emailstr = etemail.getText().toString();
+                                    String unamestr = etusuario.getText().toString();
 
-                            //insert the details in database
-                            Contact c = new Contact();
-                            c.setName(namestr);
-                            c.setApp(appstr);
-                            c.setApm(apmstr);
-                            c.setEmail(emailstr);
-                            c.setUname(unamestr);
-                            c.setPass(pass1str);
-                            c.setEdad(edadint);
-                            c.setTel(telint);
-                            c.setCont1(cont1int);
-                            c.setCont2(cont2int);
-                            c.setGenero(genero);
+                                    String telstr = etTel.getText().toString();
+                                    String cont1str = etcont1.getText().toString();
+                                    String cont2str = etcont2.getText().toString();
+                                    String pass1str = etcontraseña.getText().toString();
 
-                            c.setFum("Sin dato");
-                            c.setMed("Sin dato");
-                            c.setColt("Sin dato");
-                            c.setColh("Sin dato");
-                            c.setPresu(120);
-                            c.setPunt(120);
-                            c.setRisk(120);
-                            c.setNumPac(numPac);
-                            helper.insertContact(c);
+                                    String genero = (spGeneroR.getSelectedItem().toString());
+                                    /*int edadint = Integer.parseInt(edadstr);
+                                    int telint = Integer.parseInt(telstr);
+                                    int cont1int = Integer.parseInt(cont1str);
+                                    int cont2int = Integer.parseInt(cont2str);
+                                    int numa = (int) (Math.random() * 999) + 1;*/
 
-                            Toast pass = Toast.makeText(Registro.this, "Registro exitoso", Toast.LENGTH_LONG);
-                            pass.show();
+                                    String numastr = String.valueOf((int) (Math.random() * 999) + 1);
+                                    String nomS = namestr.substring(0, 1);
+                                    String appS = appstr.substring(0, 1);
+                                    String apmS = apmstr.substring(0, 1);
+                                    String numPac = nomS + appS + apmS + edadstr + numastr;
 
-                            Intent i = new Intent(Registro.this, Bienvenido.class);
-                            startActivity(i);
+                                    datos(namestr, appstr, apmstr, edadstr, emailstr, unamestr, telstr, cont1str, cont2str, pass1str
+                                            , genero, numPac);
+                                }
+                            });
+
+                            dialogo1.show();
+
+
                         }
 
                 }
@@ -634,7 +707,51 @@ public class Registro extends AppCompatActivity {
     }
 
 
+public void datos(String namestr, String appstr, String apmstr, String edadstr, String emailstr, String unamestr
+        ,String telstr, String cont1str, String cont2str, String pass1str, String genero, String numPac)
+{
+    try
+    {
 
+
+        //insert the details in database
+        Contact c = new Contact();
+        c.setName(namestr);
+        c.setApp(appstr);
+        c.setApm(apmstr);
+        c.setEmail(emailstr);
+        c.setUname(unamestr);
+        c.setPass(pass1str);
+        c.setEdad(Integer.parseInt(edadstr));
+        c.setTel(Integer.parseInt(telstr));
+        c.setCont1(Integer.parseInt(cont1str));
+        c.setCont2(Integer.parseInt(cont2str));
+        c.setGenero(genero);
+
+        c.setFum("Sin dato");
+        c.setMed("Sin dato");
+        c.setColt("Sin dato");
+        c.setColh("Sin dato");
+        c.setPresu(120);
+        c.setPunt(120);
+        c.setRisk(120);
+        c.setNumPac(numPac);
+        helper.insertContact(c);
+
+        Toast pass = Toast.makeText(Registro.this, "Registro exitoso", Toast.LENGTH_LONG);
+        pass.show();
+
+        Intent i = new Intent(Registro.this, Bienvenido.class);
+        startActivity(i);
+    }
+    catch (Exception e)
+    {
+        Toast pass = Toast.makeText(Registro.this, "Operacion no exitosa", Toast.LENGTH_LONG);
+        pass.show();
+        finish();
+    }
+
+}
 
 
 
